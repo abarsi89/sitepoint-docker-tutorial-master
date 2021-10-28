69b1be7c7c2b3fe0bed261d6a212281b4b4c45a4
Adam_Barsi <adamb7@verifone.com>
2021-10-28 15:49:25 +0200

    Fixing

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'routes.php';
+include_once 'Bootstrap.php';
 
 use Source\Bootstrap\Bootstrap;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
